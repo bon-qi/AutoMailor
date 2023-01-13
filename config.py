9810c3aff2ecc9315f8ab1cfad0c03fe7c66249f
Qi Zhang <qi-001@outlook.com>
2023-01-13 11:02:14 +0800

    1-13-2023 add something

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 import os
+# import configargparse
 
 ## email settings
 sender = "automail_qi@163.com"
 pwd = 'UMBHTQFENFWNBBGL'
 host_server = "smtp.163.com"
 
+## TODO: request settings
 proxies = None
-
-## TODO: Add header for anti-spider.
 header = None
 
+## informational settings
 receiver = ["2308224300@qq.com"]
 title = "[自动推送] 新的观察"
